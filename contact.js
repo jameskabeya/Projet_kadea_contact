5db072ff022a14e0dccdbec64f4ae4f568e403eb
James kabeya <jamesdanielkabeya@gmail.com>
2023-12-13 18:01:38 +0200

    finished

--- contact.js
+++ contact.js
@@ -1,5 +1,5 @@
 let tab = [];
-
+let myimagecontainer = '';
 const submit = document.querySelector('#btn1');
 let inputname = document.querySelector('#name');
 let inputuser = document.querySelector('#user_name');
@@ -7,6 +7,7 @@ let inputgroupe = document.querySelector('#team');
 let number = document.querySelector('#number')
 let inputmail = document.querySelector('#mail')
 let inputbiographie = document.querySelector('#biographie')
+let renit = document.querySelector('#reset')
 
 function complet() {
    myobject = {
@@ -22,24 +23,15 @@ function complet() {
 let formule2 = document.querySelector('.formule2')
 let image;
 submit.addEventListener('click', function (e) {
-   
+
    let myobject = complet();
    let reader = new FileReader();
-    image = document.createElement('img')
+   image = document.createElement('img')
    image.className = "Galary";
-
-   reader.onload = (e) => {
-      console.log('fdsdsfsd');
-      image.src = e.target.result;
-      if (reader.DONE) {
-         alert("chargement fini");
-
-      }
-      // 
-   }
+   console.log(myimagecontainer);
+   image.src = myimagecontainer;
    validateForm();
-   reader.readAsDataURL(file);
-   myobject.galerie = image.src;
+   myobject.galerie = myimagecontainer;
 
    tab.push(myobject);
    console.log(tab);
@@ -64,7 +56,9 @@ submit.addEventListener('click', function (e) {
    let text2 = document.createElement('div')
    // text2.className = "mailbio";
    let task5 = document.createElement('label')
+   text2.className = "gmail"
    let infodiv = document.createElement('div')
+   infodiv.className = "divbio";
    let task6 = document.createElement('label')
    let font_delete = document.createElement('div')
    font_delete.className = "font_delete";
@@ -95,19 +89,75 @@ submit.addEventListener('click', function (e) {
    mycontact.appendChild(formule)
    formule2.appendChild(mycontact)
    mycontact.appendChild(font_delete)
-   let i = document.createElement('i')
-   i.setAttribute('class', "fa fa-user-pen")
+   let edit = document.createElement('i')
+   edit.setAttribute('class', "fa fa-user-pen")
    let idelete = document.createElement('a')
    idelete.setAttribute('class', "fa fa-trash-o")
    idelete.setAttribute('style', 'color:red;')
-   font_delete.appendChild(i)
+   font_delete.appendChild(edit)
    font_delete.appendChild(idelete);
    // font_delete.appendChild(i)
 
-   let btn_delete = document.createElement('button') 
+   let btn_delete = document.createElement('button')
    idelete.addEventListener('click', function () {
       mycontact.parentNode.removeChild(mycontact);
-    })
+      alert('Supimer')
+   })
+   edit.addEventListener('click', function () {
+      let indice = leng - 1;
+      let objecttoEdit = tab[indice]
+      inputname.value = objecttoEdit.pastname
+      inputuser.value = objecttoEdit.name
+      inputgroupe.value = objecttoEdit.team
+      number.value = objecttoEdit.number
+      inputmail.value = objecttoEdit.mail
+      inputbiographie.value = objecttoEdit.Bio
+
+      let button = document.querySelector('.button')
+      let btnmodify = document.createElement('button')
+      let btncancel = document.createElement('button')
+      btnmodify.className = "btnmodify";
+      btncancel.className = "btncancel";
+
+      button.querySelector('.btncancel')
+      let btnexist = button.querySelector('.btnmodify')
+      let btnexistcancel = button.querySelector('.btncancel')
+      if(btnexist){
+         button.removeChild(btnexist)
+         button.removeChild(btnexistcancel)
+      }
+
+      button.appendChild(btnmodify)
+      btnmodify.innerText = "Modifier";
+      button.appendChild(btncancel)
+      btncancel.innerText = "Annuler";
+      console.log(btnmodify);
+      submit.style.display = 'none'
+      renit.style.display = 'none'
+      
+
+      btnmodify.addEventListener('click', function () {
+         task.innerHTML = inputname.value;
+         task2.innerHTML = inputuser.value;
+         task4.innerHTML = inputgroupe.value;
+         task3.innerHTML = number.value;
+         task5.innerHTML = inputmail.value;
+         task6.innerHTML = inputbiographie.value;
+
+         btnmodify.style.display = 'none'
+         btncancel.style.display = 'none'
+         submit.style.display = 'block'
+         renit.style.display = 'block'
+      })
+
+   })
+   prenom.value = "";
+   name1.value = "";
+   telephone.value = "";
+   groupe.value = "";
+   E_mail.value = "";
+   bio.value = "";
+
 });
 // add pucture
 let inputphoto = document.querySelector('.container-photo1')
@@ -118,18 +168,39 @@ inputphoto.addEventListener('dragover', function (even) {
    even.stopPropagation();
    even.dataTransfer.dropEffect = 'copy';
 });
+let inputfile = document.querySelector('#file')
+let photofile = document.querySelector('.photofile')
+
+inputfile.addEventListener('change', function (e) {
+   let file = inputfile.files[0];
+   photoup = document.querySelector('#profil')
+   let reader = new FileReader();
+
+   reader.onload = (e) => {
+      photoup.src = e.target.result;
+      myimagecontainer = e.target.result;
+   }
+   if (reader.DONE) {
+      alert("affiché");
+      console.log(photoup);
+
+   }
+   reader.readAsDataURL(file);
+})
+
 let photoup;
 inputphoto.addEventListener('drop', function (event) {
    event.stopPropagation();
    event.preventDefault();
    inputphoto.setAttribute('style', 'borde-color:"";');
 
-    photoup = document.querySelector('#profil')
+   photoup = document.querySelector('#profil')
    file = event.dataTransfer.files[0];
    let reader = new FileReader();
 
    reader.onload = (e) => {
       photoup.src = e.target.result;
+      myimagecontainer = e.target.result;
    }
    reader.readAsDataURL(file);
 });
@@ -195,32 +266,21 @@ let groupe = document.querySelector('#team')
 let E_mail = document.querySelector('#mail')
 let bio = document.querySelector('#biographie')
 let splitimage = document.querySelector('#profil')
-let renit = document.querySelector('#reset')
 
-renit.addEventListener('click',function(){
-    prenom.value="";
-});
-renit.addEventListener('click',function(){
-   name1.value="";
-});
-renit.addEventListener('click',function(){
-    telephone.value="";
-});
-renit.addEventListener('click',function(){
-    groupe.value="";
-});
-renit.addEventListener('click',function(){
-   E_mail.value="";
-});
-renit.addEventListener('click',function(){
-    bio.value="";
-});
-renit.addEventListener('click',function(){
-   splitimage.value="";
+renit.addEventListener('click', function () {
+   prenom.value = "";
+   name1.value = "";
+   telephone.value = "";
+   groupe.value = "";
+   E_mail.value = "";
+   bio.value = "";
+   let profil = document.querySelector('.profil')
+   profil.parentNode.removeChild(profil);
+
 });
 function validateForm() {
-   if (prenom.value == "" && name1.value == "" && telephone.value =="" && groupe.value == "" && E_mail.value == ""  && image.src=="") {
-     alert("Tous les champs doivent être remplis");
+   if (prenom.value == "" && name1.value == "" && telephone.value == "" && groupe.value == "" && E_mail.value == "" && image.src == "") {
+      alert("Tous les champs doivent être remplis");
    }
-  }
-  console.log(validateForm);
+}
+
